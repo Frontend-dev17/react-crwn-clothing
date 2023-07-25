6d0577a4177fe7681b580c97d40342ed51e595d8
shreya171899 <101312679+shreya171899@users.noreply.github.com>
2023-07-25 12:49:45 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import Home from "./routes/Home/Home";
+import { Outlet, Route, Routes } from "react-router-dom";
 
-function App() {
+
+const Navigation = () => {
   return (
-    <Home />
+    <div>
+      <div>
+        <h1>Naviagtion</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+const App = () => {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />} >
+        <Route index element={<Home />} />
+        {/* <Route path="/shop" element={<Shop />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
